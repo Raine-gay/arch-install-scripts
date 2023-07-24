9e7fb17dcb50f4dce4f34545ad1f9908d6a7903a
na <na@na.com>
2023-07-24 20:49:42 +0100

    trying some shit

--- 5-user-home-manager.sh
+++ 5-user-home-manager.sh
@@ -11,6 +11,14 @@ nix-channel --update
 nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl && nix-channel --update
 nix-env -iA nixgl.auto.nixGLDefault
 
+# Clone the home-manager repo
+pushd $HOME/.config/
+git clone https://github.com/Raine-gay/home-manager.git
+
+# Stop tracking the machine-specific configs.
+pushd $HOME/.config/home-manager
+git update-index --skip-worktree _machine-specific/*
+
 # setup home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
@@ -19,3 +27,5 @@ nix-shell '<home-manager>' -A install
 # allow nonfree nix software
 mkdir $HOME/.config/nixpkgs/
 echo "{ allowUnfree = true; }" >> $HOME/.config/nixpkgs/config.nix
+
+home-manager switch
